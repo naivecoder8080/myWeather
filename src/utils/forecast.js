c9f91957bc53d5631af0a760d6387059d3b8f85c
Kumar Ankit <naivecoder8080@gmail.com>
2022-12-15 16:16:07 +0530

    Humidity feature added,changed temperature unit to Celcius.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request=require('request');
 const forecast=(latitude,longitude,callback)=>{
-const url=`http://api.weatherstack.com/current?access_key=c0cc21bad7ad5bb41c5624005a4c5af3&query=${latitude},${longitude}&units=f`
+const url=`http://api.weatherstack.com/current?access_key=c0cc21bad7ad5bb41c5624005a4c5af3&query=${latitude},${longitude}&units=m`
 request({url,json:true},(error,{body}={})=>{
     if(error)
     callback('Unable to connect to internet!');
@@ -9,7 +9,7 @@ request({url,json:true},(error,{body}={})=>{
     else
     {
        current=body.current;
-       callback(undefined,`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`);
+       callback(undefined,`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.The humidity is ${current.humidity}%.`);
 
     }
 })
